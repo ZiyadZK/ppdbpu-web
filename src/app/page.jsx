849ae10dbea8b6e770fbfd07f0a18a1fb484ea3e
Ziyad <kakangtea74@gmail.com>
2024-06-05 08:58:41 +0700

    Adding Empty Data Page for Dashboard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import MainLayoutPage from "@/components/mainLayout";
 import { date_getYear } from "@/libs/date";
 import { M_Akun_getUserdata } from "@/libs/models/M_Akun";
 import { M_Siswa_rekap } from "@/libs/models/M_Siswa";
-import { faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BarChart, LineChart } from "@mui/x-charts";
 import { useEffect, useState } from "react";
@@ -40,6 +40,7 @@ export default function Home() {
     setLoadingFetch('loading')
     const response = await M_Siswa_rekap()
     if(response.success) {
+      console.log(response.data)
       setDataRekap(response.data)
     }
     setLoadingFetch('fetched')
@@ -80,7 +81,7 @@ export default function Home() {
           <div className="loading loading-spinner loading-lg text-zinc-600"></div>
         </div>
       )}
-      {loadingFetch === 'fetched' && (
+      {loadingFetch === 'fetched' && dataRekap !== null && (
         <div className="m-5 md:m-0">
           <div className="w-full bg-white md:p-5 md:rounded-2xl">
             <h1 className="font-medium text-2xl">
@@ -133,6 +134,11 @@ export default function Home() {
             </h1>
             <div className="w-full">
               <BarChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 xAxis={[{ scaleType: 'band', data: ['TKJ', 'DPIB', 'TKR', 'TPM', 'TITL', 'GEO'] }]}
                 series={[{ data: [dataRekap[rekapTahun]['total_terdaftar_tkj'], dataRekap[rekapTahun]['total_terdaftar_dpib'], dataRekap[rekapTahun]['total_terdaftar_tkr'], dataRekap[rekapTahun]['total_terdaftar_tpm'], dataRekap[rekapTahun]['total_terdaftar_titl'], dataRekap[rekapTahun]['total_terdaftar_geo']], label: "Total Pendaftar" }, { data: [dataRekap[rekapTahun]['total_daftarUlang_tkj'], dataRekap[rekapTahun]['total_daftarUlang_dpib'], dataRekap[rekapTahun]['total_daftarUlang_tkr'], dataRekap[rekapTahun]['total_daftarUlang_tpm'], dataRekap[rekapTahun]['total_daftarUlang_titl'], dataRekap[rekapTahun]['total_daftarUlang_geo']], label: "Sudah Daftar Ulang" }]}
                 height={300}
@@ -147,6 +153,11 @@ export default function Home() {
             </h1>
             <div className="w-full">
               <BarChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 xAxis={[{ scaleType: 'band', data: ['KETM', 'PRESTASI KEJUARAAN', 'RAPOR UNGGULAN', 'KATEGORI LAIN'] }]}
                 series={[{ data: [dataRekap[rekapTahun]['total_terdaftar_kategori_ketm'], dataRekap[rekapTahun]['total_terdaftar_kategori_kejuaraan'], dataRekap[rekapTahun]['total_terdaftar_kategori_rapor'], dataRekap[rekapTahun]['total_terdaftar_kategori_lain']]}]}
                 height={300}
@@ -165,6 +176,11 @@ export default function Home() {
               </div>
               <hr className="my-3" />
               <LineChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 series={[
                   { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar']), label: 'Total Siswa' },
                   { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_daftarUlang']), label: 'Daftar Ulang' },
@@ -183,13 +199,18 @@ export default function Home() {
               </div>
               <hr className="my-3" />
               <LineChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 series={[
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_tkj']), label: 'TKJ', color: '#22c55e' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_dpib']), label: 'DPIB', color: '#92400e' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_tkr']), label: 'TKR', color: '#2563eb' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_tpm']), label: 'TPM', color: '#f97316' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_titl']), label: 'TITL', color: '#6b7280' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_terdaftar_geo']), label: 'GEO', color: '#e11d48' }
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tkj']), label: 'TKJ', color: '#22c55e' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_dpib']), label: 'DPIB', color: '#92400e' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tkr']), label: 'TKR', color: '#2563eb' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tpm']), label: 'TPM', color: '#f97316' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_titl']), label: 'TITL', color: '#6b7280' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_geo']), label: 'GEO', color: '#e11d48' }
                 ]}
                 xAxis={[{ scaleType: 'point', data: Object.keys(dataRekap) }]}
                 height={300}
@@ -208,9 +229,14 @@ export default function Home() {
               </div>
               <hr className="my-3" />
               <LineChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 series={[
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_daftarUlang_jk_laki']), label: 'Laki - laki' },
-                  { data: Object.keys(dataRekap).map(year => dataRekap[year]['total_daftarUlang_jk_perempuan']), label: 'Perempuan' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_jk_laki']), label: 'Laki - laki' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_jk_perempuan']), label: 'Perempuan' },
                 ]}
                 xAxis={[{ scaleType: 'point', data: Object.keys(dataRekap).slice(0, 5) }]}
                 height={300}
@@ -226,12 +252,17 @@ export default function Home() {
               </div>
               <hr className="my-3" />
               <LineChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 series={[
-                  { data: [310, 343, 544, 123, 432], label: 'Orang Tua' },
-                  { data: [323, 313, 300, 341, 323], label: 'Wali' },
-                  { data: [323, 113, 400, 241, 323], label: 'Kos' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tinggal_ortu']), label: 'Orang Tua' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tinggal_wali']), label: 'Wali' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_tinggal_ortu']), label: 'Kos' },
                 ]}
-                xAxis={[{ scaleType: 'point', data: ['2020', '2021', '2022', '2023', '2024'] }]}
+                xAxis={[{ scaleType: 'point', data: Object.keys(dataRekap).slice(0, 5) }]}
                 height={300}
                 className="w-full"
               />
@@ -248,37 +279,35 @@ export default function Home() {
               </div>
               <hr className="my-3" />
               <LineChart
+                slotProps={{
+                  noDataOverlay: {
+                    message: 'Data tidak ada!'
+                  }
+                }}
                 series={[
-                  { data: [310, 343, 544, 123, 432], label: 'Angkutan Umum' },
-                  { data: [310, 343, 544, 123, 432], label: 'Antar Jemput' },
-                  { data: [310, 343, 544, 123, 432], label: 'Jalan Kaki' },
-                  { data: [310, 343, 544, 123, 432], label: 'Motor' },
-                  { data: [323, 313, 300, 341, 323], label: 'Sepeda' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_transport_angkut']), label: 'Angkutan Umum' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_transport_antar']), label: 'Antar Jemput' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_transport_jalan']), label: 'Jalan Kaki' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_transport_motor']), label: 'Motor' },
+                  { data: Object.keys(dataRekap).slice(0, 5).map(year => dataRekap[year]['total_terdaftar_transport_sepeda']), label: 'Sepeda' },
                 ]}
-                xAxis={[{ scaleType: 'point', data: ['2020', '2021', '2022', '2023', '2024'] }]}
+                xAxis={[{ scaleType: 'point', data: Object.keys(dataRekap).slice(0, 5) }]}
                 height={300}
                 className="w-full"
               />
             </div>
-            <div className="p-5 bg-white rounded-2xl w-full md:w-1/2">
-              <div className="flex items-center justify-between">
-                <h1 className="font-medium text-2xl">Total Data Siswa</h1>
-                <p className="px-3 py-2 rounded-full bg-zinc-100 text-zinc-700 font-medium">
-                  TINGGAL BERSAMA
-                </p>
-              </div>
-              <hr className="my-3" />
-              <LineChart
-                series={[
-                  { data: [310, 343, 544, 123, 432], label: 'Orang Tua' },
-                  { data: [323, 313, 300, 341, 323], label: 'Wali' },
-                  { data: [323, 113, 400, 241, 323], label: 'Kos' },
-                ]}
-                xAxis={[{ scaleType: 'point', data: ['2020', '2021', '2022', '2023', '2024'] }]}
-                height={300}
-                className="w-full"
-              />
-            </div>
+          </div>
+        </div>
+      )}
+      {loadingFetch === 'fetched' && dataRekap === null && (
+        <div className="p-5 w-full h-screen flex items-center justify-center">
+          <div className="bg-white p-5 rounded-2xl w-fit flex flex-col items-center">
+            <FontAwesomeIcon icon={faExclamationTriangle} className="w-12 h-12 text-cyan-600" />
+            <h1 className="font-bold text-3xl">Data tidak ditemukan!</h1>
+            <hr className="my-3 opacity-0" />
+            <p className="text-center">
+              Hal ini dikarenakan Data yang di rekap tidak ada didalam Database, <br /> anda bisa menambahkan <b>Data Baru</b> ke dalam Daftar Calon Siswa.
+            </p>
           </div>
         </div>
       )}
